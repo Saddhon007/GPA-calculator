96fd7d3c0d8134a2f66c6f8b5fe0a27b7d0381e5
Sadhon Chondro <sadhonchondro1997@gmail.com>
2024-04-04 23:01:43 +0600

    upload full calcualtor body

--- CustomJs/Custom.js
+++ CustomJs/Custom.js
@@ -28,19 +28,42 @@ function changeGradeFormat(format) {
 
     const grade = document.getElementById("gradeSystemBtn");
     const point = document.getElementById("pointSystemBtn");
+
     const bengaliGrade = document.getElementById("bengaliGrade");
     const bengaliMarks = document.getElementById("bengaliMarks");
+
     const englishGrade = document.getElementById("englishGrade");
     const englishMarks=document.getElementById("englishMarks");
+
     const mathGrade = document.getElementById("mathGrade");
     const mathMarks=document.getElementById("mathMarks");
+    
     const religionGrade = document.getElementById("religionGrade");
     const religionMarks=document.getElementById("religionMarks");
 
+    const BangladeshGrade = document.getElementById("BangladeshGrade");
+    const BangladeshMarks=document.getElementById("BangladeshMarks");
+
+    const technologyGrade = document.getElementById("technologyGrade");
+    const technologyMarks=document.getElementById("technologyMarks");
+
+    const physicsGrade = document.getElementById("physicsGrade");
+    const physicsMarks=document.getElementById("physicsMarks");
+
+    const chemistryGrade = document.getElementById("chemistryGrade");
+    const chemistryMarks=document.getElementById("chemistryMarks");
+
+    const biologyGrade = document.getElementById("biologyGrade");
+    const biologyMarks=document.getElementById("biologyMarks");
+
+    const optionalGrade = document.getElementById("optionalGrade");
+    const optionalMarks=document.getElementById("optionalMarks");
+
     if (format === "grade") {
+
         bengaliGrade.style = "display:block";
         bengaliMarks.style = "display:none";
-
+        
         englishGrade.style = "display:block";
         englishMarks.style="display:none";
 
@@ -49,6 +72,24 @@ function changeGradeFormat(format) {
 
         religionGrade.style = "display:block";
         religionMarks.style="display:none";
+
+        BangladeshGrade.style = "display:block";
+        BangladeshMarks.style="display:none";
+
+        technologyGrade.style = "display:block";
+        technologyMarks.style="display:none";
+
+        physicsGrade.style = "display:block";
+        physicsMarks.style="display:none";
+
+        chemistryGrade.style = "display:block";
+        chemistryMarks.style="display:none";
+        
+        biologyGrade.style = "display:block";
+        biologyMarks.style="display:none";
+
+        optionalGrade.style = "display:block";
+        optionalMarks.style="display:none";
 
         grade.style = "background:#00834c; color:white";
         point.style = "background:none";
@@ -66,13 +107,33 @@ function changeGradeFormat(format) {
         religionGrade.style = "display:none";
         religionMarks.style="display:block";
 
+        BangladeshGrade.style = "display:none";
+        BangladeshMarks.style="display:block";
+
+        BangladeshGrade.style = "display:none";
+        BangladeshMarks.style="display:block";
+
+        technologyGrade.style = "display:none";
+        technologyMarks.style="display:block";
+
+        physicsGrade.style = "display:none";
+        physicsMarks.style="display:block";
+
+        chemistryGrade.style = "display:none";
+        chemistryMarks.style="display:block";
+
+        biologyGrade.style = "display:none";
+        biologyMarks.style="display:block";
+
+        optionalGrade.style = "display:none";
+        optionalMarks.style="display:block";
+
         point.style = "background:#00834c;color:white";
         grade.style = "background:none;color:black";
     }
 }
 
 function subjectCodeChanger(code){
-    console.log(code)
     const dynamicCode=document.getElementById("dynamicCode");
    if(code === "112"){
     dynamicCode.innerHTML=code;
